9dbdfd44ddefa8f1cae447fcc823f7ce44e106d2
Harmouch101 <mahmoudddharmouchhh@gmail.com>
2020-10-03 13:30:16 -0400

    update

--- Exercises/ex07_newtonSquareRoot.py
+++ Exercises/ex07_newtonSquareRoot.py
@@ -4,6 +4,14 @@ Compute the square root of a number.
 2. The outputs are the program's estimate of the square root
 using Newton's method of successive approximations, and
 Python's own estimate using math.sqrt.
+
+• real value : 1.414235624 (sqrt(2))
+• formula: new_estimate = old_estimate/2 + x/(old_estimate * 2)
+• old_estimate = 1 # initial guess of sqrt(2) (x = 2)
+• new_estimate = old_estimate/2 + 1 = 1.5
+• new_estimate = new_estimate/2 + 2/3 = 1.41666
+• new_estimate = 0.7083 + 2/2.833 = 1.41425
+
 """
 import math
 if __name__ == "__main__":
